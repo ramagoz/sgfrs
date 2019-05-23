901380f884c5312de7d1df15197dbaaab5402f85
juanra1993 <40009713+juanra1993@users.noreply.github.com>
2019-05-22 22:55:39 -0400

    ROL

--- app/Http/Controllers/OficialControlador.php
+++ app/Http/Controllers/OficialControlador.php
@@ -32,6 +32,15 @@ class OficialControlador extends Controller
 
     }
 
+        public function datatablerol()
+    {
+         $persona_rol= DB::table('personas')->get();
+         //return Datatables::of(Persona::query())->make(true);
+         return Datatables::of($persona_rol)->make(true);
+
+    }
+
+
 
     public function getRecuperarGrupo(request $request)
     {
