d527bda80d35cb241ef64a0adb41ba978e2138df
juanra1993 <40009713+juanra1993@users.noreply.github.com>
2019-11-10 20:24:40 -0300

    unificado estilo de recuperacion de contraseñas

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,47 +1,72 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Restablecer Contraseña</title>
+   <!--Made with love by Mutiullah Samim -->
 
-@section('content')
+    <!--Bootsrap 4 CDN-->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+    <!--Fontawesome CDN-->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+    <meta charset="utf-8">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+  
+    <!--Custom styles-->
+    <link href="{{ asset('css/styles.css') }}" rel="stylesheet">
+</head>
+<body>
+
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
+     <div class="d-flex justify-content-center h-150">
+      <div class="card bg-secondary text-white" style="max-width: 25rem;">
+        <form method="POST" action="{{ route('password.email') }}">
+          @csrf
+            <div class="text-center">
+                  <h5><i class="fa fa-lock fa-3x"></i></h5>
+                  <h2 class="text-center">Olvidaste tu Contraseña?</h2>
+                  <p>Puedes recuperarlo aquí.</p> 
+                         <div class="card-body"> 
+                           <div class="input-group form-group">
+                                  <div class="input-group-prepend">
+                                    <span class="input-group-text"><i class="fas fa-user"></i></span>
+                                  </div>
+                                  <input id="email" type="email" class="form-control {{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ $email ?? old('email') }}" required autofocus placeholder="Correo Electrónico">
+                                    @if ($errors->has('email'))
+                                        <span class="invalid-feedback">
+                                            <strong>{{ $errors->first('email') }}</strong>
+                                        </span>
+                                    @endif
+                          </div>
+                         </div>
+             </form>
+             
+                <div class="card-footer">
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
+                                 <div class= "input-group form-group">
+                                    <button type="submit" class="btn btn-success">
+                                        {{ __('Enviar link de Recuperación ') }}
+                                    </button>
+                                 </div>
+                          @if (session('status'))
+                        <div class="alert alert-success alert-dismissible" id="myAlert" >
+                           <a href="#" class="close">&times;</a>
                             {{ session('status') }}
                         </div>
                     @endif
-
-                    <form method="POST" action="{{ route('password.email') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required>
-
-                                @if ($errors->has('email'))
-                                    <span class="invalid-feedback">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Send Password Reset Link') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
                 </div>
-            </div>
-        </div>
+      </div>
     </div>
 </div>
-@endsection
+
+ <script>
+$(document).ready(function(){
+  $(".close").click(function(){
+    $("#myAlert").alert("close");
+  });
+});
+</script>
+    
+ </body>
+</html>
