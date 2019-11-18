7d9b22d16d1bc9ea4926dbd10ea2ab59762e0149
FedeCaceres16 <federicoenmanuel@gmail.com>
2019-11-18 14:00:21 -0300

    eliminacion de vistas no utilizadas

--- resources/views/empresa/baja_oficial.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts.app')
-@include('layouts.menu_empresa')
-@section('content')
-{{-- Dentro de section va el contenido de la vista--}}
-	<h3 align="center">BAJA OFICIAL DE SEGURIDAD</h1>
-
-
-@endsection
